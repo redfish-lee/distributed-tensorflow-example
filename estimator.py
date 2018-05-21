7bbc95b5209b5f44b3ca44658e60ce77dbd92c45
redfishleo <redfish.tbc@gmail.com>
2018-05-21 09:30:30 +0800

    Create estimator.py

---
+++ estimator.py
@@ -0,0 +1,157 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.examples.tutorials.mnist import input_data
+data = input_data.read_data_sets('data/MNIST/', one_hot=True)
+
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(data.train.labels)))
+print("- Test-set:\t\t{}".format(len(data.test.labels)))
+print("- Validation-set:\t{}".format(len(data.validation.labels)))
+
+data.train.cls = np.argmax(data.train.labels, axis=1)
+data.test.cls = np.argmax(data.test.labels, axis=1)
+
+# We know that MNIST images are 28 pixels in each dimension.
+img_size = 28
+
+# Images are stored in one-dimensional arrays of this length.
+img_size_flat = img_size * img_size
+
+# Tuple with height and width of images used to reshape arrays.
+img_shape = (img_size, img_size)
+
+# Number of colour channels for the images: 1 channel for gray-scale.
+num_channels = 1
+
+# Number of classes, one class for each of 10 digits.
+num_classes = 10
+
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": np.array(data.train.images)},
+    y=np.array(data.train.cls),
+    num_epochs=None,
+    shuffle=True)
+    
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": np.array(data.test.images)},
+    y=np.array(data.test.cls),
+    num_epochs=1,
+    shuffle=False)
+
+some_images = data.test.images[0:9]
+predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": some_images},
+    num_epochs=1,
+    shuffle=False)
+    
+
+def model_fn(features, labels, mode, params):
+    # Args:
+    #
+    # features: This is the x-arg from the input_fn.
+    # labels:   This is the y-arg from the input_fn,
+    #           see e.g. train_input_fn for these two.
+    # mode:     Either TRAIN, EVAL, or PREDICT
+    # params:   User-defined hyper-parameters, e.g. learning-rate.
+    
+    # Reference to the tensor named "x" in the input-function.
+    x = features["x"]
+
+    # The convolutional layers expect 4-rank tensors
+    # but x is a 2-rank tensor, so reshape it.
+    net = tf.reshape(x, [-1, img_size, img_size, num_channels])    
+
+    # First convolutional layer.
+    net = tf.layers.conv2d(inputs=net, name='layer_conv1',
+                           filters=16, kernel_size=5,
+                           padding='same', activation=tf.nn.relu)
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)
+
+    # Second convolutional layer.
+    net = tf.layers.conv2d(inputs=net, name='layer_conv2',
+                           filters=36, kernel_size=5,
+                           padding='same', activation=tf.nn.relu)
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)    
+
+    # Flatten to a 2-rank tensor.
+    net = tf.contrib.layers.flatten(net)
+    # Eventually this should be replaced with:
+    # net = tf.layers.flatten(net)
+
+    # First fully-connected / dense layer.
+    # This uses the ReLU activation function.
+    net = tf.layers.dense(inputs=net, name='layer_fc1',
+                          units=128, activation=tf.nn.relu)    
+
+    # Second fully-connected / dense layer.
+    # This is the last layer so it does not use an activation function.
+    net = tf.layers.dense(inputs=net, name='layer_fc2',
+                          units=10)
+
+    # Logits output of the neural network.
+    logits = net
+
+    # Softmax output of the neural network.
+    y_pred = tf.nn.softmax(logits=logits)
+    
+    # Classification output of the neural network.
+    y_pred_cls = tf.argmax(y_pred, axis=1)
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        # If the estimator is supposed to be in prediction-mode
+        # then use the predicted class-number that is output by
+        # the neural network. Optimization etc. is not needed.
+        spec = tf.estimator.EstimatorSpec(mode=mode,
+                                          predictions=y_pred_cls)
+    else:
+        # Otherwise the estimator is supposed to be in either
+        # training or evaluation-mode. Note that the loss-function
+        # is also required in Evaluation mode.
+        
+        # Define the loss-function to be optimized, by first
+        # calculating the cross-entropy between the output of
+        # the neural network and the true labels for the input data.
+        # This gives the cross-entropy for each image in the batch.
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,
+                                                                       logits=logits)
+
+        # Reduce the cross-entropy batch-tensor to a single number
+        # which can be used in optimization of the neural network.
+        loss = tf.reduce_mean(cross_entropy)
+
+        # Define the optimizer for improving the neural network.
+        optimizer = tf.train.AdamOptimizer(learning_rate=params["learning_rate"])
+
+        # Get the TensorFlow op for doing a single optimization step.
+        train_op = optimizer.minimize(
+            loss=loss, global_step=tf.train.get_global_step())
+
+        # Define the evaluation metrics,
+        # in this case the classification accuracy.
+        metrics = \
+        {
+            "accuracy": tf.metrics.accuracy(labels, y_pred_cls)
+        }
+
+        # Wrap all of this in an EstimatorSpec.
+        spec = tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op,
+            eval_metric_ops=metrics)
+        
+    return spec
+
+params = {"learning_rate": 1e-4}
+model = tf.estimator.Estimator(model_fn=model_fn,
+                               params=params,
+                               model_dir="./checkpoints/")
+
+model.train(input_fn=train_input_fn, steps=2000)
+result = model.evaluate(input_fn=test_input_fn)
+# {'accuracy': 0.97610003, 'global_step': 2000, 'loss': 0.076004863}
+
+print("Classification accuracy: {0:.2%}".format(result["accuracy"]))
+# Classification accuracy: 97.61%
